3f5dc341a1886bb3365a50504fd562cb2fcfb915
Erkin Unlu <erkin.unlu@monitise.com>
2014-11-10 10:14:49 +0200

    length impl but need unit tests

--- src/main/java/SAP.java
+++ src/main/java/SAP.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ERKIN on 09/11/14.
@@ -15,10 +16,23 @@ public class SAP {
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
 
-        ArrayList<Integer> ancestorV = findAncestors(v);
-        ancestorV.indexOf()
+        List<Ancestor> ancestorV = findAncestors(v);
+        List<Ancestor> ancestorW = findAncestors(w);
 
-        throw new IllegalStateException("not implemented");
+        int min = -1;
+        for (Ancestor ancestor : ancestorV) {
+            for (Ancestor other: ancestorW) {
+                if (ancestor.getIndex() == other.getIndex()) {
+                    int distance = ancestor.getDistance() + other.getDistance();
+
+                    if (min == -1 || distance < min) {
+                        min = distance;
+                    }
+                }
+            }
+        }
+
+        return min;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
@@ -39,5 +53,43 @@ public class SAP {
     // do unit testing of this class
     public static void main(String[] args) {
         throw new IllegalStateException("not implemented");
+    }
+
+    private List<Ancestor> findAncestors(int vertex) {
+        List<Ancestor> ancestors = new ArrayList<Ancestor>();
+        Ancestor root = new Ancestor(vertex, 0);
+        ancestors.add(root); // it is the closest ancestor to itself
+
+        Queue<Ancestor> bfsQueue = new Queue<Ancestor>();
+        bfsQueue.enqueue(root);
+
+        while (!bfsQueue.isEmpty()) {
+            Ancestor vtx = bfsQueue.dequeue();
+            for (int index : digraph.adj(vtx.getIndex())) {
+                Ancestor ancestor = new Ancestor(index, vtx.getDistance() + 1);
+                bfsQueue.enqueue(ancestor);
+                ancestors.add(ancestor);
+            }
+        }
+
+        return ancestors;
+    }
+
+    private static class Ancestor {
+        private final int index;
+        private final int distance;
+
+        private Ancestor(int index, int distance) {
+            this.index = index;
+            this.distance = distance;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
     }
 }
